c940192c6688e6dcd61bcf31f13645359e3787e6
Ирина Сичкарева <0702irisha@mail.ru>
2023-02-28 14:48:36 +0200

    на ревью3

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -114,7 +114,9 @@ def follow_index(request):
 @login_required
 def profile_follow(request, username):
     author = get_object_or_404(User, username=username)
-    if request.user != author:
+    following = Follow.objects.filter(
+        author=author, user=request.user).exists()
+    if request.user != author and not following:
         Follow.objects.create(
             user=request.user,
             author=author
